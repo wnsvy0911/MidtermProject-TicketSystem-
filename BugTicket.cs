56a16551a25dc5231058543a98aad4c1689b248b
Junpyo Kim <jpkimkorea@gmail.com>
2022-03-15 15:18:08 -0500

    Create BugTicket class

---
+++ BugTicket.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace TicketingSystem
+{
+    class BugTicket: Ticket
+    {
+
+        public string severity {get; set;}
+         public BugTicket (
+            int ticketId,
+            string summary,
+            string status,
+            string priority,
+            string submitter,
+            string assigned,
+            List<string> watching,
+            string severity) : base (ticketId, summary, status, priority, submitter, assigned, watching)
+        {
+           this.severity = severity;
+        }
+
+
+
+    }
+} 
